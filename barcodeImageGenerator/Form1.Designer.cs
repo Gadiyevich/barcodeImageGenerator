cd0198cff2189a5e51e460cf60e0ff6668f656e7
gadiyevich <48290063+gadiyevich@users.noreply.github.com>
2019-08-11 23:33:16 +0400

    Add project files.

---
+++ barcodeImageGenerator/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace barcodeImageGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBarcode = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.BtnGenerate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBarcode = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBarcode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBarcode
+            // 
+            this.picBarcode.BackColor = System.Drawing.Color.White;
+            this.picBarcode.Location = new System.Drawing.Point(12, 12);
+            this.picBarcode.Name = "picBarcode";
+            this.picBarcode.Size = new System.Drawing.Size(917, 388);
+            this.picBarcode.TabIndex = 0;
+            this.picBarcode.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(688, 413);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 36);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // BtnGenerate
+            // 
+            this.BtnGenerate.Location = new System.Drawing.Point(818, 413);
+            this.BtnGenerate.Name = "BtnGenerate";
+            this.BtnGenerate.Size = new System.Drawing.Size(86, 36);
+            this.BtnGenerate.TabIndex = 2;
+            this.BtnGenerate.Text = "Generate";
+            this.BtnGenerate.UseVisualStyleBackColor = true;
+            this.BtnGenerate.Click += new System.EventHandler(this.BtnGenerate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 413);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Barcode:";
+            // 
+            // txtBarcode
+            // 
+            this.txtBarcode.Location = new System.Drawing.Point(94, 406);
+            this.txtBarcode.Multiline = true;
+            this.txtBarcode.Name = "txtBarcode";
+            this.txtBarcode.Size = new System.Drawing.Size(554, 29);
+            this.txtBarcode.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(948, 479);
+            this.Controls.Add(this.txtBarcode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnGenerate);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.picBarcode);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Barcode Image";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBarcode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBarcode;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button BtnGenerate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBarcode;
+    }
+}
+
---
+++ barcodeImageGenerator/Form1.cs
@@ -0,0 +1,57 @@
+﻿using BarcodeLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace barcodeImageGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnGenerate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                Barcode barcode = new Barcode();
+                Color foreColor = Color.Black;
+                Color backColor = Color.Transparent;
+                Image image = barcode.Encode(TYPE.UPCA, txtBarcode.Text, foreColor, backColor, (int)(picBarcode.Width * 0.8), (int)(picBarcode.Height * 0.8));
+                picBarcode.Image = image;
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Length must be 11 or 12 numbers!","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog save=new SaveFileDialog() { Filter="PNG|*.png" } )
+            {
+                if (save.ShowDialog()==DialogResult.OK)
+                {
+                    if (picBarcode.Image == null)
+                        return;
+                    picBarcode.Image.Save(save.FileName);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
